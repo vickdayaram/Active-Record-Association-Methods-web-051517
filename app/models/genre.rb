241f760842e7955067b5e919a011a52382aef016
github username <github email address>
2017-05-25 09:15:53 -0400

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,20 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.length
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    art_genres = []
+    self.songs.each do |song|
+      art_genres.push(song.artist)
+    end
+    art_genres.uniq.length
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    self.songs.map {|song| song.artist.name}
   end
 end
